7a79bae29d5e619ade46508f067b4ccce75ca746
Jarad DeLorenzo <jaradd@gmail.com>
2012-12-06 18:27:42 -0500

    Initial Commit

---
+++ Thermite/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  ThermiteAppController.h
+//  Thermite
+//
+//  Created by Jarad Delorenzo on 12/6/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
