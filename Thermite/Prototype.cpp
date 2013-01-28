e2fffd3d8979a30a3efb9f2c11b6c42a22cd8d03
delorenj <jaradd@gmail.com>
2013-01-27 21:11:19 -0500

    Implmented proper non-convex polygon bomb blast placement relative to localPoint click

--- Thermite/Prototype.cpp
+++ Thermite/Prototype.cpp
@@ -55,7 +55,7 @@ void Prototype::testSimple() {
 }
 
 void Prototype::testSeparator() {
-	b2Separator* sep = new b2Separator();
+	b2Separator sep;
     vector<b2Vec2>* vec = new vector<b2Vec2>();
     vec->push_back(b2Vec2(-4, -4));
     vec->push_back(b2Vec2(4, -4));
@@ -67,7 +67,7 @@ void Prototype::testSeparator() {
 	m_bodyDef.position.Set((m_centerPoint.x-300)/PTM_RATIO, m_centerPoint.y/PTM_RATIO);
 	b2Body* body = getWorld()->CreateBody(&m_bodyDef);
 
-    sep->Separate(body, &m_fixtureDef, vec, PTM_RATIO);
+    sep.Separate(body, &m_fixtureDef, vec, PTM_RATIO);
 
 	//	Only need below to attach box2d body to a cocos2d sprite...
 	PhysicsSprite* ps = new PhysicsSprite();
@@ -79,12 +79,12 @@ void Prototype::testSeparator() {
 }
 
 void Prototype::testPlaceBomb(b2Body* body, const CCPoint touchPoint, const float radius) {
-	b2Separator* sep = new b2Separator();
+	b2Separator sep;
 	CCLog("Breaking Body: %d", static_cast<PhysicsSprite*>(body->GetUserData())->getTag());
 	vector<b2Vec2>* bombShape = generateBlastShape(radius);
 
 	try{
-		switch(sep->Validate(*bombShape)) {
+		switch(sep.Validate(*bombShape)) {
 		case 1:
 			throw(new exception("Overlapping lines"));
 			break;
@@ -104,7 +104,11 @@ void Prototype::testPlaceBomb(b2Body* body, const CCPoint touchPoint, const floa
 
 	b2Vec2 worldPoint =  b2Vec2(touchPoint.x/PTM_RATIO, touchPoint.y/PTM_RATIO);
 	b2Vec2 localPoint = body->GetLocalPoint(worldPoint);
-	sep->Separate(body, &m_fixtureDef, bombShape, PTM_RATIO);
+	for(int i=0; i<bombShape->size(); i++) {
+		bombShape->at(i).x += localPoint.x;
+		bombShape->at(i).y += localPoint.y;
+	}
+	sep.Separate(body, &m_fixtureDef, bombShape, PTM_RATIO);
 	//m_fixtureDef.shape = bombShape;
 	//b2Fixture* bomb = body->CreateFixture(&m_fixtureDef);
 	for(b2Fixture* fix = body->GetFixtureList(); fix; fix=fix->GetNext()) {
@@ -130,13 +134,10 @@ vector<b2Vec2>* Prototype::generateBlastShape(float radius) {
 	vec->push_back(b2Vec2(-1.7f, -1.35f));
 	vec->push_back(b2Vec2(1.3f, -2.7f));
 	vec->push_back(b2Vec2(1.1f, -1.2f));
-	vec->push_back(b2Vec2(0.0f, -1.0f));
-
-	//vec->push_back(b2Vec2(2.6f, 0));
-	//vec->push_back(b2Vec2(1.4f, 2.0f));
-	//vec->push_back(b2Vec2(0, 1.3f));
-	//vec->push_back(b2Vec2(-2.0f, 1.0f));
-
+	vec->push_back(b2Vec2(2.6f, 0));
+	vec->push_back(b2Vec2(1.4f, 2.0f));
+	vec->push_back(b2Vec2(0, 1.3f));
+	vec->push_back(b2Vec2(-2.0f, 1.0f));
 	return vec;
 }
 
